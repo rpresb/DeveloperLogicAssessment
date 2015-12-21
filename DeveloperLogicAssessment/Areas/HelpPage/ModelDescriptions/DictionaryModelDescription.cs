5d42430d4ac0c6a0f8432b3db0b069e7c2846e29
Rodrigo De Presbiteris <rpresb@hotmail.com>
2015-12-21 00:37:13 -0200

    Removing useless files

--- DeveloperLogicAssessment/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace DeveloperLogicAssessment.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- DeveloperLogicAssessment/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace DeveloperLogicAssessment.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
