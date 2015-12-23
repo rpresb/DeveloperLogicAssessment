bd3d097f2ab49dd09bec6f556267141e3d2e3db5
Rodrigo De Presbiteris <rpresb@hotmail.com>
2015-12-23 02:10:29 -0200

    Removing trash

--- DeveloperLogicAssessment.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace DeveloperLogicAssessment.Web.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- DeveloperLogicAssessment.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DeveloperLogicAssessment.Web.WebApiApplication" Language="C#" %>
